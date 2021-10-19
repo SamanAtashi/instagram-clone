0b4b550ae084765a766377ae30e66f84f78b3876
SamanAtashi <milady00126@gmail.com>
2021-10-19 21:17:04 +0300

    Add image as nextjs tag to header

---
+++ Components/Header.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+function Header() {
+	return (
+		<div>
+			<div>
+				<Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png" layout="fill" />
+			</div> 
+		</div>
+	);
+}
+
+export default Header;
