431a1f33c27ed171a6ad095c96ebcb0db00579a8
SamanAtashi <milady00126@gmail.com>
2021-10-21 13:51:03 +0300

    Add header of each post

--- Components/Feed/Posts/Post.js
+++ Components/Feed/Posts/Post.js
@@ -1,9 +1,27 @@
-function Post() {
-    return (
-        <div>
-            
-        </div>
-    )
+import {
+	BookmarkIcon,
+	ChatIcon,
+	DotsHorizontalIcon,
+	EmojiHappyIcon,
+	HeartIcon,
+	PaperAirplaneIcon,
+} from '@heroicons/react/outline';
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
+
+function Post({ key, username, userImg, img, caption }) {
+	return (
+		<div className="bg-white  my-7 border rounded-sm">
+			<div className="flex items-center p-5">
+				<img
+					className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+					src={userImg}
+					alt="profile pic"
+				/>
+				<p className="flex-1 font-bold">{username}</p>
+				<DotsHorizontalIcon className="h-5" />
+			</div>
+		</div>
+	);
 }
 
-export default Post
+export default Post;
