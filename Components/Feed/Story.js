85437dc745965f7f268438c5a3a682c336aebfbc
SamanAtashi <milady00126@gmail.com>
2021-10-21 09:25:22 +0300

    Add style to Story

--- Components/Feed/Story.js
+++ Components/Feed/Story.js
@@ -1,8 +1,14 @@
 function Story({ username, img }) {
 	return (
 		<div>
-			<img src={img} alt="profile" />
-			<p>{username}</p>
+			<img
+				className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-150 ease-out "
+				src={img}
+				alt="profile"
+			/>
+			<p className="text-sm w-14 truncate text-center">
+				{username}
+			</p>
 		</div>
 	);
 }
