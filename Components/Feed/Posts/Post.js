b50f5f4ab72cffa8715417ffc14636cee1199162
SamanAtashi <milady00126@gmail.com>
2021-10-21 13:55:33 +0300

    Add main image to each post

--- Components/Feed/Posts/Post.js
+++ Components/Feed/Posts/Post.js
@@ -11,6 +11,9 @@ import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
 function Post({ key, username, userImg, img, caption }) {
 	return (
 		<div className="bg-white  my-7 border rounded-sm">
+			{
+				// ! header--------------------------------
+			}
 			<div className="flex items-center p-5">
 				<img
 					className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
@@ -20,6 +23,22 @@ function Post({ key, username, userImg, img, caption }) {
 				<p className="flex-1 font-bold">{username}</p>
 				<DotsHorizontalIcon className="h-5" />
 			</div>
+			{
+				// ! main img -----------------------------
+			}
+            <img src={img} alt="post image" className="object-cover w-full"/>
+			{
+				// ! Btns ---------------------------------
+			}
+			{
+				// ! Captions -----------------------------
+			}
+			{
+				// ! Comments -----------------------------
+			}
+			{
+				// ! input post ---------------------------
+			}
 		</div>
 	);
 }
