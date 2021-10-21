dab42fe128a2a20cc2eedabdd8f989ab884a4b34
SamanAtashi <milady00126@gmail.com>
2021-10-21 10:11:34 +0300

    Add Posts files

--- Components/Feed/Feed.js
+++ Components/Feed/Feed.js
@@ -1,11 +1,12 @@
-import Stories from './Stories';
+import Posts from './Posts/Posts';
+import Stories from './Stories/Stories';
 
 function Feed() {
 	return (
 		<main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
 			<section className="col-span-2">
 				<Stories />
-				{/* Posts */}
+			    <Posts />
 			</section>
 			<section>
 				{/* mini Profile */}
---
+++ Components/Feed/Posts/Posts.js
@@ -0,0 +1,6 @@
+import Post from './Post';
+function Posts() {
+	return <div></div>;
+}
+
+export default Posts;
