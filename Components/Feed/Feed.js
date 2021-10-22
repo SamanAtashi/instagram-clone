2583f1e3df09deedd469d5d095c961038af03635
SamanAtashi <milady00126@gmail.com>
2021-10-22 09:09:23 +0300

    Add miniprofile

--- Components/Feed/Feed.js
+++ Components/Feed/Feed.js
@@ -1,3 +1,4 @@
+import MiniProfile from './MiniProfile';
 import Posts from './Posts/Posts';
 import Stories from './Stories/Stories';
 
@@ -6,11 +7,13 @@ function Feed() {
 		<main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
 			<section className="col-span-2">
 				<Stories />
-			    <Posts />
+				<Posts />
 			</section>
-			<section>
-				{/* mini Profile */}
-				{/* suggestions */}
+			<section className="hidden xl:inline-grid md:col-span-1">
+				<div className="fixed top-20">
+					<MiniProfile />
+					{/* suggestions */}
+				</div>
 			</section>
 		</main>
 	);
---
+++ Components/Feed/MiniProfile.js
@@ -0,0 +1,22 @@
+function MiniProfile() {
+	return (
+		<div className="flex items-center justify-between mt-14 ml-10">
+			<img
+				src="https://icon-library.com/images/user-image-icon/user-image-icon-11.jpg"
+				alt="profile picture"
+				className="w-16 h-16 rounded-full border p-[2px]"
+			/>
+			<div className="flex-1 mx-4">
+				<h2 className="font-bold">saman</h2>
+				<h3 className="text-sm text-gray-400">
+					Welcome to Instagram
+				</h3>
+			</div>
+			<button className="text-blue-400 text-sm font-semibold">
+				Sign Out
+			</button>
+		</div>
+	);
+}
+
+export default MiniProfile;
